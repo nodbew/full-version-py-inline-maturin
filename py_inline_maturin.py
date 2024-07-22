f4443f114f2fd458dd7febfb180c3e3adce348c9
nodbew <165175929+nodbew@users.noreply.github.com>
2024-07-22 15:20:18 +0900

    Update py_inline_maturin.py

--- py_inline_maturin.py
+++ py_inline_maturin.py
@@ -1,10 +1,47 @@
 import subprocess
+from subprocess import run
 from pathlib import Path
+import os
 
-def create_new_maturin(name: str|Path) -> bool:
-    if Path(str(name)).is_dir:
+def create_maturin_project(name: str) -> None:
+    '''
+    Create a new maturin project with the desired name.
+    '''
+
+    if type(name) != str:
+        raise TypeError(f'The "name" argument must be a str instance, not {type(name)}')
+        
+    # Check overlappings
+    if Path('./' + name).is_dir():
         raise FileExistsError(f"Directory {name} already exists; please choose another name")
+
+    # Install rust and maturin
+    run("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y", shell = True)
+    print('Successfully installed rust:')
+    run("rustup --version")
+    run("pip install -U maturin", shell = True)
+
+    # Create directory
+    Path('./' + name).mkdir(parents = True, exist_ok = True)
+    os.chdir('./' + name)
+
+    # Maturin new
+    run(f"maturin new --name {name} --bindings pyo3 .", shell = True)
+    os.chdir('..')
     
-    subprocess.run("pip install -U maturin", shell = True)
-    subprocess.run(f"maturin init {name}")
-    return True
+    return 
+
+def build_maturin_project(path: str|Path) -> None:
+    if not isinstance(path, (str, path)):
+        raise TypeError(f'The "path" argument must be a str or pathlib.Path object, not {type(path)}')
+
+    # Search for the directory under the current directory
+    if not Path('./' + str(path)).is_dir():
+        raise FileNotFoundError(f"There is no directory named {path}")
+
+    # Develop
+    os.chdir('./' + str(path))
+    run("maturin develop")
+    os.chdir('..')
+
+    return 
