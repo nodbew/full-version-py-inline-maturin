67cd020df72a4651fac6e6946f517efc0de69724
nodbew <165175929+nodbew@users.noreply.github.com>
2024-07-23 07:53:04 +0900

    test_py_inline_maturin.py を更新

--- test_py_inline_maturin.py
+++ test_py_inline_maturin.py
@@ -20,23 +20,14 @@ def test_creation():
     return
 
 # Test function
-@pytest.mark.parametrize(('cmd','funcname'), [
-    ('''
+def test_build():
+    cmd = '''
 use pyo3::prelude::*;
 
 #[pyfunction]
 fn add_two(num: i64) -> i64 {
     num + 2
 }
-
-#[pymodule]
-fn pymodtest(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(add_two, m)?)?;
-    Ok(())
-}
-    ''','add_two'),
-    ('''
-use pyo3::prelude::*;
 
 #[pyfunction]
 fn split_string(s: &str) -> Vec<&str> {
@@ -49,26 +40,23 @@ fn split_string(s: &str) -> Vec<&str> {
 
 #[pymodule]
 fn pymodtest(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(add_two, m)?)?;
     m.add_function(wrap_pyfunction!(split_string, m)?)?;
     Ok(())
 }
-    ''','split_string'),
-])
-def test_build(cmd, funcname):
-    
+'''
     if not Path('./pymodtest').is_dir():
-        # Initialization for the next test 
+        # Initialization of the working dir
         py_inline_maturin.create_maturin_project('pymodtest')
     
     with open('./pymodtest/src/lib.rs', 'w', encoding = 'utf-8') as f:
         f.write(cmd)
+        
     py_inline_maturin.build_maturin_project('pymodtest')
     import pymodtest
-    match funcname:
-        case 'add_two':
-            assert pymodtest.add_two(6) == 8
-        case 'split_string':
-            assert type(pymodtest.split_string("hello ")) == list
-            assert [t.strip() for t in pymodtest.split_string("Hello, world! I'm Macintosh.")] == ['Hello,', 'world!', "I'm", 'Macintosh.']
+    
+    assert pymodtest.add_two(6) == 8
+    assert type(pymodtest.split_string("hello ")) == list
+    assert [t.strip() for t in pymodtest.split_string("Hello, world! I'm Macintosh.")] == ['Hello,', 'world!', "I'm", 'Macintosh.']
 
     return
