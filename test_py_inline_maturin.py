8ae822a083fedea0e192ece984d22f68ccccd46d
nodbew <165175929+nodbew@users.noreply.github.com>
2024-07-24 17:05:48 +0900

    test_py_inline_maturin.py を更新

--- test_py_inline_maturin.py
+++ test_py_inline_maturin.py
@@ -25,12 +25,12 @@ def test_build():
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn add_two(num: i64) -> i64 {
+pub fn add_two(num: i64) -> i64 {
     num + 2
 }
 
 #[pyfunction]
-fn split_string(s: &str) -> Vec<&str> {
+pub fn split_string(s: &str) -> Vec<&str> {
     let mut result = vec![];
     for part in s.split_whitespace() {
         result.push(part);
@@ -39,7 +39,7 @@ fn split_string(s: &str) -> Vec<&str> {
 }
 
 #[pymodule]
-fn pymodtest(m: &Bound<'_, PyModule>) -> PyResult<()> {
+pub fn pymodtest(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(add_two, m)?)?;
     m.add_function(wrap_pyfunction!(split_string, m)?)?;
     Ok(())
