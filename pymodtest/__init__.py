0d658f5c58c2b2f98120bfdb9bcd7cf6b49ae8a0
nodbew <165175929+nodbew@users.noreply.github.com>
2024-07-24 23:03:16 +0900

    __init__.py を更新

--- pymodtest/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-msg = ""
-msg += f"attributes: {dir(sys.modules[__name__])}\n"
-for attr in dir(sys.modules[__name__]):
-    msg += f"{attr}: {getattr(sys.modules[__name__], attr)}\n"
-raise Exception(msg)
