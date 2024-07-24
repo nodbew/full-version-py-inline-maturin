f94504271ff5bd811ab93ec4dcac337e8fba3f62
nodbew <165175929+nodbew@users.noreply.github.com>
2024-07-24 16:50:38 +0900

    py_inline_maturin.py を更新

--- py_inline_maturin.py
+++ py_inline_maturin.py
@@ -38,7 +38,7 @@ def build_maturin_project(path: str|Path) -> None:
 
     # Develop
     os.chdir('./' + str(path))
-    run("maturin develop")
+    run("maturin develop --verbose")
     os.chdir('..')
 
     return 
