a1f300ce9e8e344413b9945e5204f3f447669c6e
nodbew <165175929+nodbew@users.noreply.github.com>
2024-08-04 06:44:57 +0900

    test_py_inline_maturin.py を更新

--- maturin_test/test_py_inline_maturin.py
+++ maturin_test/test_py_inline_maturin.py
@@ -25,7 +25,7 @@ def test_build():
     py_inline_maturin.initialize_maturin_project('maturin_test/pymodtest')
     py_inline_maturin.build_maturin_project('maturin_test/pymodtest')
     
-    from . import pymodtest
+    import pymodtest
     assert pymodtest.add_two(6) == 8
     assert type(pymodtest.split_string("hello ")) == list
     assert [t.strip() for t in pymodtest.split_string("Hello, world! I'm Macintosh.")] == ['Hello,', 'world!', "I'm", 'Macintosh.']
