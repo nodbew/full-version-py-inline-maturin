56bec10bd93db21a902e5dd9da7415abde6922ce
nodbew <165175929+nodbew@users.noreply.github.com>
2024-08-06 09:14:07 +0900

    lib.rs を更新

--- maturin_test/pymodtest/src/lib.rs
+++ maturin_test/pymodtest/src/lib.rs
@@ -7,12 +7,14 @@ fn add_two(num: i64) -> PyResult<i64> {
 
 #[pyfunction]
 fn split_string(s: &str) -> PyResult<Vec<String>> {
-    Ok(s.split_whitespace().map(|part| part.to_string()).collect())
+    Ok(s.split_whitespace()
+            .map(|part| part.to_string())
+            .collect())
 }
 
 #[pymodule]
 fn pymodtest(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    println!("{:?}", m.add_function(wrap_pyfunction!(add_two, m)?));
-    println!("{:?}", m.add_function(wrap_pyfunction!(split_string, m)?)?);
+    m.add_function(wrap_pyfunction!(add_two, m)?)?;
+    m.add_function(wrap_pyfunction!(split_string, m)?)?;
     Ok(())
 }
