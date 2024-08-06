beec077e54876712910591e8049304241e5b0a1d
nodbew <165175929+nodbew@users.noreply.github.com>
2024-08-06 13:59:12 +0900

    test_py_inline_maturin.py を更新

--- maturin_test/test_py_inline_maturin.py
+++ maturin_test/test_py_inline_maturin.py
@@ -2,24 +2,8 @@ import os
 from pathlib import Path
 from shutil import rmtree
 
-import pytest
-
 import py_inline_maturin
 
-def test_creation():
-    current_directory = os.getcwd()
-    py_inline_maturin.create_maturin_project(name = "creation_test")
-    if os.getcwd() != current_directory:
-        raise ValueError("Current directory changed")
-    if not Path('./creation_test').exists():
-        raise FileNotFoundError("The directory wasn't successfully created")
-
-    # Clean up
-    rmtree('./creation_test')
-    
-    return
-
-# Test function
 def test_build():
     # Initialization of the working dir
     py_inline_maturin.initialize_maturin_project('maturin_test/pymodtest')
