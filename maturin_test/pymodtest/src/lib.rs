1dd4ee684a533dd081e16807da8a80fd11a70f1a
nodbew <165175929+nodbew@users.noreply.github.com>
2024-08-06 09:30:34 +0900

    lib.rs を更新

--- maturin_test/pymodtest/src/lib.rs
+++ maturin_test/pymodtest/src/lib.rs
@@ -1,20 +1,17 @@
 use pyo3::prelude::*;
 
-#[pyfunction]
-fn add_two(num: i64) -> PyResult<i64> {
-    Ok(num + 2)
-}
-
-#[pyfunction]
-fn split_string(s: &str) -> PyResult<Vec<String>> {
-    Ok(s.split_whitespace()
-            .map(|part| part.to_string())
-            .collect())
-}
-
 #[pymodule]
-fn pymodtest(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(add_two, m)?)?;
-    m.add_function(wrap_pyfunction!(split_string, m)?)?;
-    Ok(())
+mod pymodtest {
+    
+    #[pyfunction]
+    fn add_two(num: i64) -> i64 {
+        num + 2
+    }
+    
+    #[pyfunction]
+    fn split_string(s: &str) -> Vec<String> {
+        s.split_whitespace()
+            .map(|part| part.to_string())
+            .collect()
+    }
 }
