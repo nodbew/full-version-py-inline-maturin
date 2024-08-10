9e1890ef4b4517137cbeeaea4c4b84b6dcebe11a
nodbew <165175929+nodbew@users.noreply.github.com>
2024-08-10 17:25:23 +0900

    py_inline_maturin.py を更新

--- maturin_test/py_inline_maturin.py
+++ maturin_test/py_inline_maturin.py
@@ -12,30 +12,38 @@ def run(cmd: str, **kwargs) -> subprocess.CompletedProcess:
     return subprocess.run(cmd, shell = True, check = True, **kwargs)
     
 
-def initialize_maturin_project(path: str, create: bool = False, name: str = None) -> None:
+def initialize_maturin_project(path: str | Path = None, create: bool = False, name: str = None) -> None:
     
     '''Edit pyproject.toml and Cargo.toml to the maturin project style.'''
 
-    # Name defaults to the directory name
-    if name is None:
+    # name and path defaults to each other
+    if path is None:
+        if name is None:
+            raise ValueError("Either path or name has to be passed")
+        else:
+            path = f"./{name}"
+    
+    elif name is None:
         name = list(str(path).split("/"))[-1] # last directory in the path
     
+    # create or look for the directory
     if create:
-        run(f"maturin new -b pyo3 ./{path}")
+        run(f"maturin new -b pyo3 {path}")
     else:
-        if not Path(f"./{path}").exists():
-            raise FileNotFoundError(f"The directory('{Path(f'./{path}').resolve()}') does not exist")
+        if not Path(str(path)).exists():
+            raise FileNotFoundError(f"The directory('{Path(str(path)).resolve()}') does not exist")
 
-    toml_util.edit_pyproject_toml(path = f"./{path}/pyproject.toml", name = name)
-    toml_util.edit_cargo_toml(path = f"./{path}/pyproject.toml", name = name)
+    # Edit configuration
+    toml_util.edit_pyproject_toml(path = f"{path}/pyproject.toml", name = name)
+    toml_util.edit_cargo_toml(path = f"{path}/pyproject.toml", name = name)
 
     # look for src directory and lib.rs in it
-    if Path(f'./{name}/src').is_dir():
-        if not Path(f'./{name}/src/lib.rs').is_file():
-            Path(f'./{name}/src/lib.rs').touch()
+    if Path(f'{path}/src').is_dir():
+        if not Path(f'{path}/src/lib.rs').is_file():
+            Path(f'{path}/src/lib.rs').touch()
     else:
-        Path(f'./{name}/src').mkdir()
-        Path(f'./{name}/src/lib.rs').touch()
+        Path(f'{path}/src').mkdir()
+        Path(f'{path}/src/lib.rs').touch()
         
     return
 
